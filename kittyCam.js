41ee9a96f7aa379dbd912e7acb412316f00d1873
Emerik <emerik.bedouin@ecole.ensicaen.fr>
2018-01-27 10:32:09 +0000

    [ADD] add pubnub listener to trigger photo capture and start/stop auto capture

--- kittyCam.js
+++ kittyCam.js
@@ -10,6 +10,7 @@
 const config = require('./config');
 const fs = require('fs');
 const child_process = require('child_process');
+var PubNub = require('pubnub');
 
 require('events').EventEmitter.prototype._maxListeners = 20;
 
@@ -18,13 +19,15 @@ const raspi = require('raspi-io');
 const five = require('johnny-five');
 const board = new five.Board({io: new raspi()});
 
+//Global variables
 let i = 0;
 let maxPicture = 25;
 let lastPicture = Date.now();
 let lastHour = -1;
+let doRecognition = false;
+let status = true;
 
 //Check args
-let doRecognition = false;
 console.log('User arg : '+process.argv[2]);
 if (process.argv[2] == 'cat') doRecognition = true ;
 
@@ -60,50 +63,22 @@ board.on('ready', () => {
       lastHour = new Date().getHours();
       return;
     }
-    console.log(new Date().getHours()+' : '+lastHour);
+
     if(new Date().getHours() == lastHour){
       console.log('Too many pictures this hour '+lastHour+'H');
       return ;
     }
 
-    // Run raspistill command to take a photo with the camera module
-    let filename = 'photo/image_'+i+'.jpg';
-    let args = ['-w', '1024', '-h', '780', '-o', filename, '-t', '1000'];
-    let spawn = child_process.spawn('raspistill', args);
+    if(status == false){
+      console.log('Auto mode is off');
+      return;
+    }
 
-    spawn.on('exit', (code) => {
-      console.log('A photo is saved as '+filename+ ' with exit code, ' + code);
-      let timestamp = Date.now();
-      i++;
+    // Take picture & upload it
+    takePicture(i);
+    i++;
 
-      if (doRecognition) {
-      // Detect cats from photos
-
-        if((/jpg$/).test(filename)) { // Ignore the temp filenames like image_001.jpg~
-          let imgPath = __dirname + '/' + filename;
-
-          // Child process: read the file and detect cats with KittyDar
-          let args = [imgPath];
-          let fork = child_process.fork(__dirname + '/detectCatsFromPhoto.js');
-          fork.send(args);
-
-          // the child process is completed
-          fork.on('message', (base64) => {
-            if(base64) {
-              uploadToCloudinary(base64, timestamp);
-            }
-
-            // Once done, delete the photo to clear up the space
-            deletePhoto(imgPath);
-          });
-        }
-      }
-      else {
-        uploadToCloudinary(filename, timestamp);
-        deletePhoto(filename); //TODO review
-      }
-
-    })
+    
   });
 
   // 'motionend' events
@@ -122,13 +97,52 @@ function deletePhoto(imgPath) {
   });
 }
 
+function takePicture(inc){
+  // Run raspistill command to take a photo with the camera module
+  let filename = 'photo/image_'+inc+'.jpg';
+  let args = ['-w', '1024', '-h', '780', '-o', filename, '-t', '1000'];
+  let spawn = child_process.spawn('raspistill', args);
+  let timestamp = Date.now();
+
+  spawn.on('exit', (code) => {
+    console.log('A photo is saved as '+filename+ ' with exit code, ' + code);
+    
+
+    if (doRecognition) {
+    // Detect cats from photos
+
+      if((/jpg$/).test(filename)) { // Ignore the temp filenames like image_001.jpg~
+        let imgPath = __dirname + '/' + filename;
+
+        // Child process: read the file and detect cats with KittyDar
+        let args = [imgPath];
+        let fork = child_process.fork(__dirname + '/detectCatsFromPhoto.js');
+        fork.send(args);
+
+        // the child process is completed
+        fork.on('message', (base64) => {
+          if(base64) {
+            uploadToCloudinary(base64, timestamp);
+          }
+
+          // Once done, delete the photo to clear up the space
+          deletePhoto(imgPath);
+        });
+      }
+    }
+    else {
+      uploadToCloudinary(filename, timestamp);
+      deletePhoto(filename); //TODO review
+    }
+  })
+}
 
 // PubNub to publish the data
 // to make a separated web/mobile interface can subscribe the data to stream the photos in realtime.
 
 const channel = 'kittyCam';
 
-const pubnub = require('pubnub').init({
+var pubnub = new PubNub({
   subscribe_key: config.pubnub.subscribe_key,
   publish_key: config.pubnub.publish_key
 });
@@ -141,6 +155,53 @@ function publish(url, timestamp) {
     error: (err) => {console.log(err);}
   });
 }
+
+function publishAcq(msg, timestamp){
+  pubnub.publish({
+    channel: channel,
+    message: {type: 'status', message: msg, timestamp: timestamp},
+    callback: (m) => {console.log(m);},
+    error: (err) => {console.log(err);}
+  });
+}
+
+pubnub.addListener({
+  status: function(statusEvent) {
+      if (statusEvent.category === "PNConnectedCategory") {
+          console.log('Status event');
+          //publishSampleMessage();
+      }
+  },
+  message: function(message) {
+      console.log('['+message.message.cmd+'] command received');
+      //What to do
+      let timestamp = Date.now();
+      switch(message.message.cmd){
+        case 'takePicture':
+          //Call picture function
+          takePicture(333);
+          break;
+        case 'stop':
+          status = false;
+          publishAcq('false', timestamp);
+          break;
+        case 'start':
+          status = true;
+          publishAcq('true', timestamp);
+          break;
+        default:
+          break;
+      }
+  },
+  presence: function(presenceEvent) {
+      // handle presence
+      console.log('Presence event');
+  }
+})      
+console.log("Subscribing..");
+pubnub.subscribe({
+  channels: [channel] 
+});
 
 // Cloudinary to store the photos
 
@@ -174,9 +235,9 @@ rl.on('SIGINT', () => {
   process.exit();
 });
 
-rl.on('line', (input) => {
+/*rl.on('line', (input) => {
   if(input == 'cat') doReco = !doReco;
   if(input == 'reset') lastHour=-1;
   console.log('Time '+new Date().toUTCString()+' LH:'+lastHour)
   console.log('Cat recognition now is  '+doRecognition);
-});
+});*/
